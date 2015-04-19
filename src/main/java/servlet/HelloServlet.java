130002047de58fac738b98417da20c84aeb48403
Velytchenko Oleksandr <avelit@ukr.net>
2015-04-19 14:39:12 +0300

    servlet

---
+++ src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+        name = "pathGoogle", 
+        urlPatterns = {"/path"}
+    )
+public class HelloServlet extends HttpServlet {
+
+  private static final long serialVersionUID = -930386857509367419L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write(("Hello! Wait for around the world service here! You start from " + req.getParameter("startPoint")).getBytes());
+        out.flush();
+        out.close();
+    }
+    
+}
